fa229f7e5ea96a93895da0447d618704deb86464
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-04-17 02:36:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp110425_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Russia("Петя"));
+            people.Add(new Ukraine("Петро"));
+            people.Add(new English("Petr"));
+
+            foreach (Person person in people)
+            {
+                person.SayHello();
+            }
+        }
+    }
+}
